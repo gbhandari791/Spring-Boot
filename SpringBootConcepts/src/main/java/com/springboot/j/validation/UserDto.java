bf347ef378fbae08e59c007b3490a9092b231412
Gaurav <gbhandari@smartsight.in>
2025-09-18 11:59:08 +0530

    Added spring boot validation example

---
+++ SpringBootConcepts/src/main/java/com/springboot/j/validation/UserDto.java
@@ -0,0 +1,56 @@
+package com.springboot.j.validation;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class UserDto {
+
+	@NotNull(message = "Id cannot be null")
+    private Integer id;
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 20, message = "Name must be between 3 and 20 characters")
+    private String name;
+
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @Min(value = 18, message = "Age must be at least 18")
+    private int age;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+    
+}
